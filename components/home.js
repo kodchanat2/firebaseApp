bd18df93c489c13f99fed75b57ef98d93509c3d4
john <kodchanat2@gmail.com>
2017-09-02 18:11:45 +0700

    feat(navbar): add router and navbar

--- components/home.js
+++ components/home.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ShowData from './showData';
 import Login from './login';
+import { Router, Scene } from 'react-native-router-flux';
 
 export default class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
         {/* <ShowData/> */}
-        <Login/>
+        <Router>
+            <Scene key="root">
+                <Scene key="login" component={Login} title="Login" initial="true" />
+                <Scene key="home" component={Home}/>
+            </Scene>
+        </Router>
       </View>
     );
   }
